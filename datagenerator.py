7ffd8268d3e43fc6985cdd192e30213a7b0e5846
Maximilian Thiessen <s6mnthie@uni-bonn.de>
2019-06-02 17:29:57 +0200

    Not working single beta data generation

--- datagenerator.py
+++ datagenerator.py
@@ -1,18 +1,122 @@
 import numpy as np
 import mapmse
+import naivelikelihood
+from scipy.spatial.distance import pdist
 
 def sample_poisson_counts_naive(list_count, parameter=None, parameter_to_lambdas=None):
     if parameter is None:
         parameter = np.zeros(1 + list_count + list_count*(list_count-1)//2)
-        parameter[0] = 20 + 2*np.random.randn()
-        parameter[1:list_count+1] = 1*np.random.randn(list_count)
-        parameter[list_count+1:] = 0.1*np.random.randn(list_count*(list_count-1)//2)
+        parameter[0] = 10 + 2*np.random.randn()
+        parameter[1:list_count+1] = -1 + np.random.randn(list_count)
+        parameter[list_count+1:] = -5 +2*np.random.randn(list_count*(list_count-1)//2)
         #parameter = np.clip(parameter, -3,3)
         #parameter /= np.abs(np.max(parameter))/10
     if parameter_to_lambdas is None:
-        parameter_to_lambdas = mapmse.get_lambda_matrix(list_count)
+        parameter_to_lambdas = naivelikelihood.get_lambda_matrix(list_count)
     lambdas = np.exp(np.dot(parameter_to_lambdas, parameter))
     return parameter, np.random.poisson(lambdas, lambdas.size)
+
+def compute_lambda_single_beta(parameter, A):
+    '''
+    Compute sum A' lambda_A' over all A' \superseteq A in a dynamic programming fashion in time O(|A|^2) instead of naive O(2^|A|)
+    :param parameter: vector containing mu, alphas and the single_beta
+    :param A: the index set A \subseteq {1,...,K}
+    :return: lambda_A
+    '''
+
+    exp_mu = np.exp(parameter[0])
+    exp_alphas = np.exp(parameter[A])
+    exp_beta = np.exp(parameter[-1])
+
+
+
+    K = exp_alphas.size
+
+    summed_lambda_A = 0
+
+    dp = np.copy(exp_alphas)  # alpha_1+...alpha_i, alpha_2+...+alpha_i ,..., alpha_i
+
+    summed_lambda_A += 1 + np.sum(dp)
+
+    for i in range(exp_alphas.size-1):
+
+        #dp[:-i] = alphas[:-i]*dp[1:K-(i-1)]
+
+        #log_lambda_A += np.power(beta, (i*(i-1))//2)*np.sum(dp[:-i])
+
+        prev_sum = 0
+
+        for j in range(K-1,i,-1):
+            prev_sum += dp[j]
+            dp[j] = exp_alphas[j-1-i]*prev_sum
+            summed_lambda_A += np.power(exp_beta, (i*(i-1))//2)*dp[j]
+
+    summed_lambda_A *= exp_mu
+    return summed_lambda_A
+
+def __sample_poisson_recursive__(parameter, K, current_population_size=0, inside=set(), outside=set(), lists=[], counts=[]):
+    candidates = list(set(range(K))-inside-outside)
+    if (len(candidates)== K):
+        #first poisson draw
+        lambda_all = compute_lambda_single_beta(parameter, range(K))
+        current_population_size = np.random.poisson(lambda_all)
+        lists = [[1] * K]
+        counts = [current_population_size]
+
+    lambdas = []
+
+    for c,i in enumerate(candidates):
+        lambdas.append(compute_lambda_single_beta(parameter, candidates[i+1:]))
+
+    subset_populations = np.random.multinomial(current_population_size, lambdas/np.sum(lambdas), size=1).flatten()
+
+    for c, i in enumerate(candidates):
+        if subset_populations[i]>0:
+            intersection_index_vector = np.zeros(K).astype(np.bool)
+            intersection_index_vector[c] = 1
+            intersection_index_vector[list(inside)] = 1
+
+            lists.append(list(intersection_index_vector))
+            counts.append(subset_populations[i])
+
+            __sample_poisson_recursive__(parameter, K, current_population_size, inside|set([c]), set(candidates[:i]),lists,counts)
+
+
+
+
+
+def sample_poisson_counts_single_beta(list_count, parameter=None, parameter_to_lambdas=None):
+    '''
+    Here we force all beta_ij to be equal to one single beta
+    :param list_count:
+    :param parameter:
+    :param parameter_to_lambdas:
+    :return:
+    '''
+    if parameter is None:
+        parameter = np.zeros(1 + list_count + 1)
+        parameter[0] = 10 + 2*np.random.randn() #mu
+        parameter[1:-1] = -1 + np.random.randn(list_count) #alphas
+        parameter[-1] = -5 +2*np.random.randn() # beta
+
+
+    __sample_poisson_recursive__(parameter, list_count)
+
+
+
+#betas decrease with distance
+def sample_poisson_counts_map_style(list_count):
+    coordinates = np.random.rand(list_count, 2)*1000
+    pairwise_distances = pdist(coordinates, 'sqeuclidean')
+    mu = np.abs(20 * np.random.randn())
+    alphas = np.abs(np.random.randn(list_count))
+    betas = np.log(1/pairwise_distances)
+
+    parameter = np.append(np.append(mu, alphas), betas)
+
+    _, counts_exponential_representation = sample_poisson_counts_naive(list_count, parameter)
+
+
 
 
 def sample_suspect_to_list_naive(list_count=50, suspect_count=100000):
@@ -63,3 +167,8 @@ def UKData():
     list_count = 6
     total_suspect_count = np.sum(counts)
     return intersection_indexer, counts, list_count, total_suspect_count, counts_exponential_representation
+
+
+compute_lambda_single_beta(np.array([2,1,2,3,4,1]), np.array([1,2,3,4]))
+
+sample_poisson_counts_single_beta(6)
