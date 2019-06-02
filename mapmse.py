2351d86e06ccf0d60ae1faed2510a400dbf18ebf
Maximilian Thiessen <s6mnthie@uni-bonn.de>
2019-06-02 17:31:03 +0200

    heuristic (skip likelihood) vs normal mle

--- mapmse.py
+++ mapmse.py
@@ -6,15 +6,28 @@ from scipy.optimize import minimize
 import skiplikelihood
 import naivelikelihood
 from naivelikelihood import neg_likelihood_naive, neg_derrivative_naive
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+plt.style.use('seaborn')
 
 def synthetic_data_experiments():
-    correct = 0
+    correct_naive = 0
+    correct_approx = 0
     experiment_count = 100
+
+    error = 0.05
+
+    accuracies_naive = []
+    accuracies_approx = []
+
+
     for i in range(experiment_count):
-        N = 15
+        N = 10
         param_count = 1 + N + N * (N - 1) // 2
         true_params, counts_exp = datagenerator.sample_poisson_counts_naive(N)
-        # np.seterr(all='raise')
+        counts = counts_exp[counts_exp > 0]
+        #np.seterr(all='raise')
         x = np.random.randn(param_count)
         enforce_zeros = np.zeros(x.size).astype(bool)
         # enforce_zeros[15:] = True
@@ -22,14 +35,150 @@ def synthetic_data_experiments():
         # x[0] = 9.39
         # print(true_params)
         # print(counts_exp)
-        fitted_exp_mu = naivelikelihood.optimize_naive(counts_exp, N, x, enforce_zeros)
-        if np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0]) < 0.05:
-            correct += 1
-        print(np.exp(true_params[0]), fitted_exp_mu,
-              np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0]),
-              np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0]) < 0.05)
+        fitted_exp_mu,_,_,_ = naivelikelihood.optimize_naive(counts_exp, N, x, enforce_zeros)
+        fitted_exp_mu = np.exp(fitted_exp_mu.x[0])
 
-    print(correct / experiment_count)
+        accuracy_naive = np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0])
+        accuracies_naive.append(accuracy_naive)
+        if accuracy_naive < error:
+            correct_naive += 1
+
+        ##skip version heuristic
+        counts_indexer = naivelikelihood.get_index_matrix_alphas_only(N).T[:,1:]
+        counts_indexer = counts_indexer[counts_exp > 0]
+        A, beta_count = skiplikelihood.construct_parameter_matrix(counts_indexer)
+
+        mu = np.random.rand()
+        alphas = np.zeros(N)  # -10*np.ones(N)#np.random.rand(N)
+        betas = np.zeros(beta_count)  # np.random.rand(beta_count)
+
+        x = np.append(np.append(mu, alphas), betas)
+
+        approx_exp_mu = minimize(skiplikelihood.neg_likelihood_vectorized, x, (A, counts), method='L-BFGS-B',
+                           jac=skiplikelihood.neg_derivative_vectorized).x[0]
+        approx_exp_mu = np.exp(approx_exp_mu)
+        accuracy_approx = np.abs(np.exp(true_params[0]) - approx_exp_mu) / np.exp(true_params[0])
+        accuracies_approx.append(accuracy_approx)
+        if accuracy_approx < error:
+            correct_approx += 1
+
+        print("%.2f; %.2f %.2f %r; %.2f %.2f %r" % (np.exp(true_params[0]), \
+                    fitted_exp_mu, np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0]), accuracy_naive < error,\
+                    approx_exp_mu, np.abs(np.exp(true_params[0]) - approx_exp_mu) / np.exp(true_params[0]), accuracy_approx < error))
+
+
+
+    print(correct_naive / experiment_count)
+    print(correct_approx / experiment_count)
+
+    fig, ax = plt.subplots()
+
+
+    stats.probplot(accuracies_naive, dist=stats.uniform, plot=plt)
+    stats.probplot(accuracies_approx, dist=stats.uniform, plot=plt)
+
+    # Remove the regression lines
+    ax.get_lines()[1].remove()
+    ax.get_lines()[2].remove()
+    # Change colour of scatter
+    ax.get_lines()[0].set_markerfacecolor('C0')
+    ax.get_lines()[1].set_markerfacecolor('C1')
+    ax.get_lines()[0].set_label("Full Optimization")
+    ax.get_lines()[1].set_label("Heuristic")
+    ax.get_lines()[0].set_markersize(5.0)
+    ax.get_lines()[1].set_markersize(5.0)
+    #ax.axvline(x=correct_naive / experiment_count, color='C0')
+    #ax.axvline(x=correct_approx / experiment_count, color='C1')
+    plt.yscale('log')
+    plt.ylabel("Relative Error")
+    plt.xlabel("Quantile")
+    ax.legend()
+    plt.savefig("test.png")
+
+    def synthetic_data_experiments():
+        correct_naive = 0
+        correct_approx = 0
+        experiment_count = 100
+
+        error = 0.05
+
+        accuracies_naive = []
+        accuracies_approx = []
+
+        for i in range(experiment_count):
+            N = 10
+            param_count = 1 + N + N * (N - 1) // 2
+            true_params, counts_exp = datagenerator.sample_poisson_counts_naive(N)
+            counts = counts_exp[counts_exp > 0]
+            # np.seterr(all='raise')
+            x = np.random.randn(param_count)
+            enforce_zeros = np.zeros(x.size).astype(bool)
+            # enforce_zeros[15:] = True
+            # x[enforce_zeros] = 0
+            # x[0] = 9.39
+            # print(true_params)
+            # print(counts_exp)
+            fitted_exp_mu, _, _, _ = naivelikelihood.optimize_naive(counts_exp, N, x, enforce_zeros)
+            fitted_exp_mu = np.exp(fitted_exp_mu.x[0])
+
+            accuracy_naive = np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(true_params[0])
+            accuracies_naive.append(accuracy_naive)
+            if accuracy_naive < error:
+                correct_naive += 1
+
+            ##skip version heuristic
+            counts_indexer = naivelikelihood.get_index_matrix_alphas_only(N).T[:, 1:]
+            counts_indexer = counts_indexer[counts_exp > 0]
+            A, beta_count = skiplikelihood.construct_parameter_matrix(counts_indexer)
+
+            mu = np.random.rand()
+            alphas = np.zeros(N)  # -10*np.ones(N)#np.random.rand(N)
+            betas = np.zeros(beta_count)  # np.random.rand(beta_count)
+
+            x = np.append(np.append(mu, alphas), betas)
+
+            approx_exp_mu = minimize(skiplikelihood.neg_likelihood_vectorized, x, (A, counts), method='L-BFGS-B',
+                                     jac=skiplikelihood.neg_derivative_vectorized).x[0]
+            approx_exp_mu = np.exp(approx_exp_mu)
+            accuracy_approx = np.abs(np.exp(true_params[0]) - approx_exp_mu) / np.exp(true_params[0])
+            accuracies_approx.append(accuracy_approx)
+            if accuracy_approx < error:
+                correct_approx += 1
+
+            print("%.2f; %.2f %.2f %r; %.2f %.2f %r" % (np.exp(true_params[0]), \
+                                                        fitted_exp_mu,
+                                                        np.abs(np.exp(true_params[0]) - fitted_exp_mu) / np.exp(
+                                                            true_params[0]), accuracy_naive < error, \
+                                                        approx_exp_mu,
+                                                        np.abs(np.exp(true_params[0]) - approx_exp_mu) / np.exp(
+                                                            true_params[0]), accuracy_approx < error))
+
+        print(correct_naive / experiment_count)
+        print(correct_approx / experiment_count)
+
+        fig, ax = plt.subplots()
+
+        stats.probplot(accuracies_naive, dist=stats.uniform, plot=plt)
+        stats.probplot(accuracies_approx, dist=stats.uniform, plot=plt)
+
+        # Remove the regression lines
+        ax.get_lines()[1].remove()
+        ax.get_lines()[2].remove()
+        # Change colour of scatter
+        ax.get_lines()[0].set_markerfacecolor('C0')
+        ax.get_lines()[1].set_markerfacecolor('C1')
+        ax.get_lines()[0].set_label("Full Optimization")
+        ax.get_lines()[1].set_label("Heuristic")
+        ax.get_lines()[0].set_markersize(5.0)
+        ax.get_lines()[1].set_markersize(5.0)
+        # ax.axvline(x=correct_naive / experiment_count, color='C0')
+        # ax.axvline(x=correct_approx / experiment_count, color='C1')
+        plt.yscale('log')
+        plt.ylabel("Relative Error")
+        plt.xlabel("Quantile")
+        ax.legend()
+        plt.savefig("test.png")
+
 
 def exponential_representation_UKData_experiments():
     intersection_indexer, counts, list_count, total_suspect_count, counts_exponential_representation = datagenerator.UKData()
@@ -70,4 +219,5 @@ def first_try_UKData():
 
 if __name__ == '__main__':
     first_try_UKData()
+    synthetic_data_experiments()
 
